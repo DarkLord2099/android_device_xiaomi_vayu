53bb2ebab9a189670298c6d0a9c53de32778fc51
mateoalfaro <73367365+mateoalfaro@users.noreply.github.com>
2024-01-10 21:33:26 -0600

    Welcome apollo

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_vayu.mk
+    $(LOCAL_DIR)/apollo_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_vayu-eng \
-    aosp_vayu-userdebug \
-    aosp_vayu-user
+    apollo_vayu-eng \
+    apollo_vayu-userdebug \
+    apollo_vayu-user
--- apollo_vayu.mk
+++ apollo_vayu.mk
@@ -11,16 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common PixelBuilds configurations
-$(call inherit-product, vendor/pb/config/common_full_phone.mk)
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
+# Inherit common Apollo stuff
+$(call inherit-product, vendor/apollo/config/common_full_phone.mk)
+
+# Gapps
+TARGET_STOCK_GAPPS := true
+
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
 
-PRODUCT_NAME := aosp_vayu
+PRODUCT_NAME := apollo_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
