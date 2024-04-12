fcb24453f6a34f01558617f2956cfe08866340b8
Dark Lord <149946506+DarkLord2099@users.noreply.github.com>
2024-04-12 05:16:01 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/banana_vayu.mk
+    $(LOCAL_DIR)/voltage_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    banana_vayu-eng \
-    banana_vayu-userdebug \
-    banana_vayu-user
+    voltage_vayu-eng \
+    voltage_vayu-userdebug \
+    voltage_vayu-user
